1c376fd773163cdc7dd1fa5e53188c29eee76c80
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-05-31 01:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 let div = document.getElementById("browser-info");
 
-let browserName = navigator.userAgent;
-let version = navigator.appVersion;
+let browserName = navigator.appVersion;
+let version = navigator.appName;
 // let version = " OPR/95.0.0.0";
 // let print = alert("You are using " + browserName)
-div.innerHTML = "You are using "   +version;
+div.innerHTML = `You are using ${version} ${browserName}`

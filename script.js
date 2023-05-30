ea99c52a0e1e596d84861abc34ccca8ce052c355
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-05-31 02:33:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //your JS code here. If required.
 let div = document.getElementById("browser-info");
 
-let browserName = navigator.userAgent;
-let version = navigator.appName;
+ function browserinfo(){
+	let browserName = navigator.userAgent;
+	 div.innerHTML = `You are using ${browserName}`
+}
+	browserinfo();
 
-div.innerHTML = `You are using ${browserName}`
-document.body.append(div);
+
